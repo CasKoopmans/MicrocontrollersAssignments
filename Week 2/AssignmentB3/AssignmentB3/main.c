c47d72b8c61180298f86422b0b42b33f1d4bdf8a
Cas Koopmans <cjwkoopm@avans.nl>
2017-02-15 12:57:44 +0100

    Assignment B3

---
+++ Week 2/AssignmentB3/AssignmentB3/main.c
@@ -0,0 +1,18 @@
+/*
+ * AssignmentB3.c
+ *
+ * Created: 12/02/2017 17:01:36
+ * Author : Cas
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
